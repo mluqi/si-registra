cebd8ed9d3e05e5368cb159aad4690e0ae36bf21
mluqi <mluqi12@gmail.com>
2025-10-23 09:13:27 +0700

    update .env

--- backend/services/googleSheet.js
+++ backend/services/googleSheet.js
@@ -1,13 +1,25 @@
-// services/googleSheet.js
 const { google } = require("googleapis");
 const path = require("path");
 
-async function getSheet() {
-  const auth = new google.auth.GoogleAuth({
-    keyFile: path.join(__dirname, "../credentials.json"),
-    scopes: ["https://www.googleapis.com/auth/spreadsheets"],
-  });
+async function getAuth() {
+  if (process.env.GOOGLE_CREDENTIALS_JSON) {
+    const credentials = JSON.parse(process.env.GOOGLE_CREDENTIALS_JSON);
+    return new google.auth.GoogleAuth({
+      credentials, // Gunakan objek credentials
+      scopes: ["https://www.googleapis.com/auth/spreadsheets"],
+    });
+  } else {
+    const path = require("path");
+    const KEYFILEPATH = path.join(__dirname, "../credentials.json");
+    return new google.auth.GoogleAuth({
+      keyFile: KEYFILEPATH,
+      scopes: ["https://www.googleapis.com/auth/spreadsheets"],
+    });
+  }
+}
 
+async function getSheet() {
+  const auth = await getAuth();
   const client = await auth.getClient();
   const sheets = google.sheets({ version: "v4", auth: client });
   return sheets;
